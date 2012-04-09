84a17053975328ad08dd953384cf7328949bcc41
ikhwanhayat <ikhwanhayat@gmail.com>
2012-04-09 23:54:08 +0800

    Rename project to Vaccine in AssemblyInfo.cs files

--- examples/Vaccine.Office.Web/Properties/AssemblyInfo.cs
+++ examples/Vaccine.Office.Web/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MediuCms.Office.Web")]
+[assembly: AssemblyTitle("Vaccine.Office.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MediuCms.Office.Web")]
+[assembly: AssemblyProduct("Vaccine.Office.Web")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
